67b94c5074f74a2e2e5a54415dff359ab9fb61b1
ImnotEdMateo <edmateo@incel.email>
2025-08-07 19:12:32 -0500

    refactor: more environment variables
    - Add `GUESTBOOK_PORT` and `GUESTBOOK_ALLOWED_ORIGINS`

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"os"
+	"log"
   "net/http"
+	"strings"
 
   "github.com/ImnotEdMateo/guestbook/db"
   "github.com/ImnotEdMateo/guestbook/routes"
@@ -10,9 +13,23 @@ import (
 )
 
 func main () {
+	port := os.Getenv("GUESTBOOK_PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	origins := os.Getenv("GUESTBOOK_ALLOWED_ORIGINS")
+	if origins == "" {
+		origins = "http://localhost"
+	}
+
+	allowedOrigins := strings.Split(origins, ",")
+
   db.DBConnect()
   db.DBMigrate()
-  
+
+	log.Printf("Corriendo servidor en http://0.0.0.0:%s", port)
+
   r := mux.NewRouter()
   r.HandleFunc("/", routes.GetEntriesHandler).Methods("GET")
   r.HandleFunc("/", routes.PostEntryHandler).Methods("POST")
@@ -20,11 +37,11 @@ func main () {
 
 
   handler := cors.New(cors.Options{
-		AllowedOrigins:   []string{"https://edmateo.site", "http://localhost"},
+		AllowedOrigins:   allowedOrigins,
 		AllowedMethods:   []string{"GET", "POST"},
 		AllowedHeaders:   []string{"Content-Type"},
 		AllowCredentials: true,
 	}).Handler(r)
   
-  http.ListenAndServe(":3000", handler)
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
